fe9ef57c1dfe5c20ec8135547ffd6f270554165d
amirahamzaoui <amira.hamzaoui@keepartner.com>
2015-08-11 13:27:33 +0100

    add_fields

--- keedemo/jobseeker/models.py
+++ keedemo/jobseeker/models.py
@@ -14,7 +14,7 @@ class Jobseeker (models.Model):
     nom = models.CharField(max_length=200)
     sexe = models.CharField(choices=SEXE_GENDER, max_length=1)
     date_naissance = models.DateField(auto_now=True)
-    etat_civil = models.CharField(choices=ETAT_CIVIL_CHOICES, max_length=1)
+    etat_civil = models.CharField(choices=ETAT_CIVIL_CHOICES, max_length=22)
     nationalite = models.CharField(max_length=200, blank = True)
     adresse = models.CharField(max_length=200,blank = True)
     code_postal = models.CharField(blank = True,max_length = 200)
@@ -23,6 +23,9 @@ class Jobseeker (models.Model):
     permis_conduire = models.BooleanField(default = False)
     titulaire_voitre = models.BooleanField(default = False)
     avoir_handicap =  models.BooleanField(default = False)
+
+    def __str__(self):              # __unicode__ on Python 2
+        return self.nom
 
 class CvInfoPro (models.Model):
     titre_profil = models.CharField(max_length=200)
@@ -37,3 +40,6 @@ class CvInfoPro (models.Model):
     type_poste = models.CharField(max_length=200)
     lieu_travail = models.CharField(max_length=200)
     mobilite = models.CharField(max_length=200)
+
+    def __str__(self):              # __unicode__ on Python 2
+        return self.titre_profil
